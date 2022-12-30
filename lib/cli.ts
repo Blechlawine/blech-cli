dc856caee10c9262d5b4ab2833b74e1f50e09f82
Blechlawine <marczinser@gmx.de>
2022-12-30 16:18:31 +0100

    add Astro cli and ask about installing dependencies

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,5 +1,5 @@
 import chalk from "chalk";
-import { spawn } from "child_process";
+import { ChildProcess, execSync, spawn } from "child_process";
 import inquirer from "inquirer";
 import path from "path";
 import fs from "fs-extra";
@@ -9,24 +9,26 @@ export async function handleCLI(_answers: { name: string }) {
     const answers = Object.assign(
         _answers,
         await inquirer.prompt<{
-            cli: "Vite" | "Next.js" | "T3" | "Solid-start";
+            cli: "Vite" | "Next.js" | "T3" | "Solid-start" | "Astro";
         }>([
             {
                 type: "list",
                 name: "cli",
                 message: "Which cli do you want to run?",
-                choices: ["Vite", "Next.js", "T3", "Solid-start"],
+                choices: ["Vite", "Next.js", "T3", "Solid-start", "Astro"],
             },
         ])
     );
-    let subProc;
+    let subProc: ChildProcess;
     const yarnCommand = process.platform == "win32" ? "yarn.cmd" : "yarn";
+    let askForInstall = true;
     switch (answers.cli) {
         case "Vite":
             subProc = spawn(yarnCommand, ["create", "vite", answers.name], { stdio: "inherit" });
             break;
         case "Next.js":
             subProc = spawn(yarnCommand, ["create", "next-app", answers.name], { stdio: "inherit" });
+            askForInstall = false; // Next.js already asks about installing dependencies
             break;
         case "Solid-start":
             if (!fs.existsSync(path.join(cwd(), answers.name)))
@@ -38,13 +40,35 @@ export async function handleCLI(_answers: { name: string }) {
             break;
         case "T3":
             subProc = spawn(yarnCommand, ["create", "t3-app", answers.name], { stdio: "inherit" });
+            askForInstall = false; // T3 already asks about installing dependencies
+            break;
+        case "Astro":
+            subProc = spawn(yarnCommand, ["create", "astro", answers.name], { stdio: "inherit" });
+            askForInstall = false; // astro cli already asks about installing dependencies
             break;
         default:
             console.log(chalk.red("Unknown CLI"));
             process.exit(1);
     }
     subProc.on("error", (err) => console.error(err));
-    subProc.on("close", (code) => {
-        console.log(`${answers.cli} CLI terminated:`, code);
+    await new Promise<void>((res) => {
+        subProc.on("close", (code) => {
+            console.log(`${answers.cli} CLI terminated:`, code);
+            res();
+        });
     });
+    if (askForInstall) {
+        const { shouldInstall } = await inquirer.prompt<{ shouldInstall: boolean }>([
+            {
+                type: "confirm",
+                name: "shouldInstall",
+                message: "Do you want to install dependencies? `yarn install` - ",
+                default: true,
+            },
+        ]);
+        if (shouldInstall) {
+            console.log(chalk.greenBright("Installing dependencies"));
+            execSync(`${yarnCommand} install`, { cwd: path.join(cwd(), answers.name), stdio: "inherit" });
+        }
+    }
 }
