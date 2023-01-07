6c8f034a3e4f22f4d2f6902719829a989c781b4d
Blechlawine <marczinser@gmx.de>
2023-01-07 12:22:10 +0100

    fix tailwind not working - vue template

--- templates/vue/tailwind.config.cjs
+++ templates/vue/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/*.{vue,ts}", "index.html"],
+    content: ["./src/**/*.{vue,ts}", "index.html"],
     theme: {
         extend: {},
     },
