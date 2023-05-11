e6cb1a7d3602e8ec8952f8e8c83114e7806fb6e1
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2023-05-11 18:59:45 +0200

    feat: move trpc-server template into its own repository

--- templates/trpc-server/routes/api/trpc/[trpc].ts
+++
@@ -1,9 +0,0 @@
-import { createNitroAdapter } from "../../../lib/nitro-adapter";
-import { createContext } from "../../../lib/trpc";
-import { AppRouter } from "../../../server";
-
-export const trpc = createNitroAdapter({
-    router: AppRouter,
-    createContext,
-    endpoint: "/api/trpc",
-});
