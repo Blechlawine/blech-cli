6f0908a4b7a410196078c545e7a505487fcf9bcf
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2023-05-11 20:56:15 +0200

    feat: move nitro template into its own repository

--- templates/nitropack/index.d.ts
+++
@@ -1,5 +0,0 @@
-// This exports the response types from the route handlers
-import type { InternalApi } from "nitropack";
-export type API = InternalApi;
-
-export type RouteOutput<TRoute extends keyof API> = API[TRoute];
